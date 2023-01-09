6984e1d0b40baa03939567421d348571f1ec2900
shwetank <56911869+shwetank04@users.noreply.github.com>
2023-01-10 00:59:35 +0530

    Create FilteringController.java

---
+++ RestFulWebServices/FilteringController.java
@@ -0,0 +1,32 @@
+package com.rest.webservices.restfulwebservices.filtering;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+
+@RestController
+public class FilteringController {
+
+	@GetMapping("/filtering")
+	public MappingJacksonValue filtering() {
+		// Dynamic Filtering
+		Somebean someBean = new Somebean("value1", "value2", "value3");
+		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1");
+		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+		mappingJacksonValue.setFilters(filters);
+		return mappingJacksonValue;
+	}
+
+	@GetMapping("/filtering-list")
+	public List<Somebean> filteringList() {
+		return Arrays.asList(new Somebean("value1", "value2", "value3"), new Somebean("value4", "value5", "value6"));
+	}
+}
